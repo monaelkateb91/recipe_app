946853bf25317cc27944d440b185f1aeaecdcabe
monaelkateb <mona.elkate91@gmail.com>
2023-12-27 14:18:17 +0200

    Merge branch 'master' of https://github.com/monaelkateb91/recipe_app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,59 +19,73 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
         body: Center(
-            child: Column(
+            child: SizedBox(width: 450, height: 300,
+              child: Stack(
           children: [
-            CarouselSlider(
-              carouselController:carouselControllerEx ,
-                items: [
-                  Image.asset(
-                    'assets/images/Cinnamon Toaast copy.png',
-                  ),
-                  Image.asset(
-                    'assets/images/detail_GlazedSalmon_2 copy.png',
-                  ),
-                  Image.asset(
-                    'assets/images/brown-eggs.jpg',
-                  ),
-                  Image.asset(
-                    'assets/images/frensh toast 2 copy.png',
-                  )
-                ].map((i) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  );
-                }).toList(),
-                options: CarouselOptions(
-                    height: 200,
-                    autoPlay: true,
-                    viewportFraction: 0.75,
-                    enlargeStrategy: CenterPageEnlargeStrategy.height,
-                    enlargeCenterPage: true,
-                    onPageChanged: (index, _) {
-                      sliderIndex=index;
-                      setState(() {
+              CarouselSlider(
+                carouselController:carouselControllerEx ,
+                  items: [
+                    Image.asset(
+                      'assets/images/Cinnamon Toaast copy.png',
+                    ),
+                    Image.asset(
+                      'assets/images/detail_GlazedSalmon_2 copy.png',
+                    ),
+                    Image.asset(
+                      'assets/images/brown-eggs.jpg',
+                    ),
+                    Image.asset(
+                      'assets/images/frensh toast 2 copy.png',
+                    )
+                  ].map((i) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    );
+                  }).toList(),
+                  options: CarouselOptions(
+                      height: 200,
+                      autoPlay: true,
+                      viewportFraction: 0.75,
+                      enlargeStrategy: CenterPageEnlargeStrategy.height,
+                      enlargeCenterPage: true,
+                      onPageChanged: (index, _) {
+                        sliderIndex=index;
+                        setState(() {
 
-                      });
-                      print('${index}');
-                    })),
-            DotsIndicator(
-              dotsCount: 4,
-              position: sliderIndex,
-              onTap: (position) async{
-               await carouselControllerEx.animateToPage(position);
-               sliderIndex:position;
-               setState(() {
-
-               });
+                        });
+                        print('${index}');
+                      })
+              ), Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(onPressed: (){
+                carouselControllerEx.previousPage();
               },
-              decorator: DotsDecorator(
-                  size: const Size.square(9.0),
-                  activeShape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0))),
-            )
+              icon: Icon(Icons.arrow_back),),
+            ),
+              Align(alignment: Alignment.centerRight,
+              child: IconButton(onPressed: (){
+                carouselControllerEx.nextPage();
+              },
+              icon: Icon(Icons.arrow_forward),),),
+              DotsIndicator(
+                dotsCount: 4,
+                position: sliderIndex,
+                onTap: (position) async{
+                 await carouselControllerEx.animateToPage(position);
+                 sliderIndex:position;
+                 setState(() {
+
+                 });
+                },
+                decorator: DotsDecorator(
+                    size: const Size.square(9.0),
+                    activeShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              )
           ],
-        )),
+        ),
+            )),
       ),
     );
   }
